0a9b706959941985cb12e07ece6ae822be82fb58
David <D_machio@hotmail.com>
2023-11-23 11:15:07 +0100

    a separar ramas

--- src/components/Banneraccesible/Banneraccesible.jsx
+++ src/components/Banneraccesible/Banneraccesible.jsx
@@ -11,7 +11,7 @@ const Banneraccesible = () => {
         <img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700137174/portadaaccesibles_ssn9fw.jpg" alt="" />
         <div className="text-banneraccesible">
           <h3>Más de 500 campings accesibles</h3>
-          <p>Busca entre una gran colección de campings accesibles, catalogada por tipo de accesibilidad</p>
+
           <Navlink word="Explorar" url="/campings" />
         </div>
 
