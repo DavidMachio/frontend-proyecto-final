f204668f3553d7beb5f7e14e54c64a1c27452b25
fareh <fareh.rv@gmail.com>
2023-11-27 00:42:50 +0100

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,21 @@
 import "./Login.css"
 
 const Login  = () => {
+
+    const localData =  localStorage.getItem("theme")
+
     return (
-        <main className="Login">
-            <section className="boxform">
-                <div className="logname">Log in</div>
-                <form>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" />
-                    <label htmlFor="password">Password</label>
-                    <input type="password"  />
-                    <button>Log in</button>
+        <main className="login">
+            <section className={`boxform ${localData == "light" ? "formday" : "formnight"}`}>
+                <h2 className="logname">Welcome back!</h2>
+                <form className="formsection">
+                    <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="text" placeholder="Username"/>
+                    <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="password" placeholder="Password" />
+                    <button className={`formbtn ${localData == "light" ? "btnday" : "btnnight"}`}>Log in</button>
                 </form>
+                <article className="orsect"><div className="line"></div><span className="ortext">or</span>
+                <div className="line"></div></article>
+                <p className="regline">Don´t have an account yet? <a className="linked" href="#null">Sign up!</a></p>
             </section>
         </main>
     )
