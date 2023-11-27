8e60de65ba151e43fb38add639c5f733630419ec
fareh <fareh.rv@gmail.com>
2023-11-27 13:15:54 +0100

    register login done /conection ..

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,46 @@
 import "./Login.css"
+import { useState } from "react"
 
 const Login  = () => {
 
+    const [registerPage, setRegisterPage] = useState(false)
+    const Setpage = () => {
+        setRegisterPage(!registerPage)
+    }
+    
     const localData =  localStorage.getItem("theme")
 
     return (
-        <main className="login">
-            <section className={`boxform ${localData == "light" ? "formday" : "formnight"}`}>
+        <main className="forms">
+            {registerPage == false ? (
+                <section className={`boxform ${localData == "light" ? "formday" : "formnight"}`}>
                 <h2 className="logname">Welcome back!</h2>
                 <form className="formsection">
                     <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="text" placeholder="Username"/>
                     <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="password" placeholder="Password" />
                     <button className={`formbtn ${localData == "light" ? "btnday" : "btnnight"}`}>Log in</button>
                 </form>
-                <article className="orsect"><div className="line"></div><span className="ortext">or</span>
-                <div className="line"></div></article>
-                <p className="regline">Don´t have an account yet? <a className="linked" href="#null">Sign up!</a></p>
-            </section>
+                <article className="orsect"><div className={`${localData == "light" ? "line" : "linenight"}`}></div><span className="ortext">or</span>
+                <div className={`${localData == "light" ? "line" : "linenight"}`}></div></article>
+                <p className="regline">Don´t have an account yet?</p><span onClick={Setpage} className="linked"  >Sign up!</span>
+            </section>) 
+            : 
+            (<section className={`boxform ${localData == "light" ? "formday" : "formnight"}`}>
+                <h2 className="regname">Create your account!</h2>
+                <form  className="formsection">
+                <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="text" placeholder="name" minLength={5}/>
+                <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="text" placeholder="Username" minLength={5}/>
+                <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="email" placeholder="example@once.com" pattern=".+@once\.com" title="Please provide only a ONCE corporate email address" required/>
+                <label htmlFor="avatar" className={`${localData == "light" ? "inputday" : "inputnight"}`}>
+                    Profile picture
+                </label>
+                <input type="file" id="avatar" className="inputfile" />
+                <input className={`${localData == "light" ? "inputday" : "inputnight"}`} type="password" placeholder="Password" required />
+                <button className={`formbtn ${localData == "light" ? "btnday" : "btnnight"}`}>Register</button>
+                </form>
+                <span onClick={Setpage} className="linked" >Sign up!</span>
+            </section>)
+            }
         </main>
     )
 }
