c62ca8ed193f0cef41d5001f2e8808fbd8e5b28d
fareh <fareh.rv@gmail.com>
2023-12-01 13:46:01 +0100

    some functions done

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import { useState } from 'react'
 
 const App = () => {
 
+  const [theme, setTheme] = useState(true)
 
+  const changeTheme = () => {
+    setTheme(!theme)
+  }
 
   return (
-    <>
-      <Header />
+    <div className={theme == true ? "day" : "night"}>
+      <Header action={changeTheme}/>
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
