6df77416981f32e628f23a2e05c2b51bf338fbf9
David <D_machio@hotmail.com>
2023-12-03 15:58:50 +0100

    formulario contacto repetido

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -2,7 +2,18 @@ import "./Contacto.css"
 
 const Contacto = () => {
   return (
-    <main>Contacto</main>
+    <main className="main-contacto">
+      <section className="datos">datos</section>
+      <section className="form-container-contacto">
+        <form className="form-contacto" action="https://formsubmit.co/paginawebcampings@gmail.com" method="POST">
+          <input type="text" name="Nombre" placeholder="Nombre" />
+          <input type="email" name="email" placeholder="email" />
+          <input type="text" name="Asunto" placeholder="Asunto" />
+          <textarea name="Comentario" cols="15" rows="5"></textarea>
+          <input type="submit" value="Enviar" />
+        </form>
+      </section>
+    </main>
   )
 }
 export default Contacto;
