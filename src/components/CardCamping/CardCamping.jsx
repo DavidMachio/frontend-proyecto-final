1e4d1cb0b5daf88b94cfe826f3bf2270cf2335ab
fareh <fareh.rv@gmail.com>
2023-12-07 17:21:05 +0100

    logos

--- src/components/CardCamping/CardCamping.jsx
+++ src/components/CardCamping/CardCamping.jsx
@@ -35,16 +35,16 @@ const CardCamping = ({ data, entorno }) => {
 
           {accesible == true ? (
             <>
-              {data.type.visual.adaptado == true ? <li className="icono-accesibilidad"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081952/ciego_t0oz9k.png" alt="Icono accesibilidad visual" /></li> : ""}
-              {data.type.auditivo.adaptado == true ? <li className="icono-accesibilidad"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081464/auditiva_q9lwfk.png" alt="Icono accesibilidad auditiva" /></li> : ""}
-              {data.type.movilidad.adaptado == true ? <li className="icono-accesibilidad"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081467/movilidad_hpttok.png" alt="Icono accesibilidad de movilidad" /></li> : ""}
+              {data.type.visual.adaptado == true ? <li className="icono-accesibilidad ico-acc-night"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081952/ciego_t0oz9k.png" alt="Icono accesibilidad visual" /></li> : ""}
+              {data.type.auditivo.adaptado == true ? <li className="icono-accesibilidad ico-acc-night"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081464/auditiva_q9lwfk.png" alt="Icono accesibilidad auditiva" /></li> : ""}
+              {data.type.movilidad.adaptado == true ? <li className="icono-accesibilidad ico-acc-night"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1701081467/movilidad_hpttok.png" alt="Icono accesibilidad de movilidad" /></li> : ""}
             </>
           ) : <>
-            {data.entorno == "montaña" ? <li className="icono-entorno"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939445/montan%CC%83as_w9xmba.png" alt="Icono montañas" />
+            {data.entorno == "montaña" ? <li className="icono-entorno"><img className=" ico-acc-night" src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939445/montan%CC%83as_w9xmba.png" alt="Icono montañas" />
               <h6>{data.entorno}</h6></li> : ""}
-            {data.entorno == "playa" ? <li className="icono-entorno"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939449/playa_p2lsxl.png" alt="Icono playa" />
+            {data.entorno == "playa" ? <li className="icono-entorno"><img className=" ico-acc-night" src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939449/playa_p2lsxl.png" alt="Icono playa" />
               <h6>{data.entorno}</h6></li> : ""}
-            {data.entorno == "ciudad" ? <li className="icono-entorno"><img src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939439/ciudad_ssg5kl.png" alt="Icono montaña" />
+            {data.entorno == "ciudad" ? <li className="icono-entorno"><img className=" ico-acc-night" src="https://res.cloudinary.com/dt9uzksq0/image/upload/v1700939439/ciudad_ssg5kl.png" alt="Icono montaña" />
               <h6>{data.entorno}</h6></li> : ""}
           </>}
         </ul>
